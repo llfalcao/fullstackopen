401bcd7a41140dad281072f73497f30a56d18c90
Luiz Falcão <39093175+llfalcao@users.noreply.github.com>
2026-01-13 20:30:04 -0300

    Part 2: 2.1 - create course component

---
+++ part2/course-information/src/components/Course.jsx
@@ -0,0 +1,15 @@
+import Content from './Content';
+import Header from './Header';
+
+const Course = ({ course }) => {
+  const { name, parts } = course;
+
+  return (
+    <div>
+      <Header name={name} />
+      <Content parts={parts} />
+    </div>
+  );
+};
+
+export default Course;
