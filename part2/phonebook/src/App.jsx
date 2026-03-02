e2e58128c9c9d982efe750e3b785fb901273b21a
Luiz Falcão <39093175+llfalcao@users.noreply.github.com>
2026-03-02 20:32:38 -0300

    Part 2: 2.6 - add person to phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,20 +4,32 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
 
+  const handleInputChange = (event) => setNewName(event.target.value);
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName('');
+  };
+
   return (
     <div>
       <div>debug: {newName}</div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleInputChange} />
         </div>
         <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
